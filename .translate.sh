c975201c8cd2fc14def6c29290b935afd7eb681a
MJ <john.hervicc@gmail.com>
2021-12-18 18:38:03 +0100

    (#10) Script for generating MO translation files.

--- .translate.sh
+++ .translate.sh
@@ -4,5 +4,14 @@
 # or are defined as aliases in .bashrc.
 shopt -s expand_aliases
 
+echo "Generating translation file template."
 wmlxgettext --domain wesnoth-Saurian_Pack \
     -o translations/wesnoth-Saurian_Pack.pot --recursive
+
+declare -a supported_languages=("pl")
+for language in "${supported_languages[@]}"
+do
+    echo "Generation .mo translation file for language: $language."
+    msgfmt translations/$language/wesnoth-Saurian_Pack.po \
+        -o translations/$language/LC_MESSAGES/wesnoth-Saurian_Pack.mo
+done
