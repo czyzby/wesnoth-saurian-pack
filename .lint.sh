86102999bbb0055e1d2a32d41b17a3484b30bc18
MJ <john.hervicc@gmail.com>
2021-12-31 14:00:01 +0100

    (#16) Scripts cleanup.

--- .lint.sh
+++ .lint.sh
@@ -3,6 +3,8 @@
 # This script assumes that the Wesnoth tools are available as executables
 # or are defined as aliases in .bashrc.
 shopt -s expand_aliases
+# alias wmlindent="python3 $WESNOTH_DIR/data/tools/wmlindent"
+# alias wmllint="python3 $WESNOTH_DIR/data/tools/wmllint"
 
-wmllint .
 wmlindent .
+wmllint .
--- .translate.sh
+++ .translate.sh
@@ -3,6 +3,7 @@
 # This script assumes that the Wesnoth tools are available as executables
 # or are defined as aliases in .bashrc.
 shopt -s expand_aliases
+# alias wmlxgettext="python3 $WESNOTH_DIR/data/tools/wmlxgettext"
 
 echo "Generating translation file template."
 wmlxgettext --domain wesnoth-Saurian_Pack \
